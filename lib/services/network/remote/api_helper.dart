70f4e19c6dcb308faae494c695f1588285cd0ebb
AliSabbar <agog.emo@gmail.com>
2022-10-03 12:07:51 +0300

    second commit

--- lib/services/network/remote/api_helper.dart
+++ lib/services/network/remote/api_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:curd_app/services/network/remote/app_excption.dart';
 import 'package:http/http.dart' as http;
 
 import '../../../components/constants.dart';
@@ -12,7 +13,7 @@ class ApiHelper {
       "Content-Type": "application/json",
       "lang": 'en',
       "Authorization": TOKEN,
-    });
+    }).timeout(const Duration(seconds: 5),onTimeout: ()=>throw 'Check Your Internet');
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
@@ -32,12 +33,28 @@ class ApiHelper {
         "Authorization": TOKEN,
       },
       body: jsonEncode(body),
-    );
+    ).timeout(const Duration(seconds: 5),onTimeout: ()=>throw 'Check Your Internet');
 
     if (response.statusCode == 200) {
-      return jsonDecode(response.body);
+      return jsonResponse(response);
     } else {
       throw Exception("There are Error in Status Code ${response.statusCode}");
+    }
+  }
+  static jsonResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = json.decode(response.body.toString());
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occurred while Communication with Server with StatusCode : ${response.statusCode}');
     }
   }
 }
